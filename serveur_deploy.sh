c429023387352183d16c9bd660eabd656990364a
hachim-soutra <hachimsoutra10@gmail.com>
2021-09-26 00:41:43 +0200

    test

--- serveur_deploy.sh
+++ serveur_deploy.sh
@@ -1,28 +1,5 @@
 git fetch origin main
 git reset --hard origin/main
-
-read -p "Do you want to upadte composer ? (y,n)" yn
-case $yn in
-    [Yy])
-        echo start  upadte composer..
-        php -d memory_limit=-1 composer.phar update
-    ;;
-    *)
-        echo No migration..
-    ;;
-esac
-
-
-read -p "Do you wish to migarte? (y,n)" yn
-case $yn in
-    [Yy])
-        echo start migration..
-
-        php artisan migrate
-    ;;
-    *)
-        echo No migration...
-    ;;
-esac
-
+php -d memory_limit=-1 composer.phar update
+php artisan migrate
 echo "Application deployed!"

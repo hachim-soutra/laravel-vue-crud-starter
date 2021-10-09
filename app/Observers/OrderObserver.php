8a449c91c890238394721bd5ce9caacd6e687873
hachim-soutra <hachimsoutra10@gmail.com>
2021-10-09 14:21:27 +0200

    testg

--- app/Observers/OrderObserver.php
+++ app/Observers/OrderObserver.php
@@ -40,13 +40,13 @@ class OrderObserver
                 $produit->save();
             }
 
-            if($order->status_livraison_id === 2 && $order->getOriginal('status_livraison_id') != 1 && $order->getOriginal('status_livraison_id') != 6 ){
+            if($order->getOriginal('status_livraison_id') === 2 && $order->status_livraison_id != 1 && $order->status_livraison_id != 6 ){
                 $produit = $order->product;
                 $produit->quantity += $order->quantity;
                 $produit->save();
             }
 
-            if($order->status_livraison_id === 6 && $order->getOriginal('status_livraison_id') != 1 && $order->getOriginal('status_livraison_id') != 1 ){
+            if($order->getOriginal('status_livraison_id') === 6 && $order->status_livraison_id != 1 && $order->status_livraison_id != 1 ){
                 $produit = $order->product;
                 $produit->quantity += $order->quantity;
                 $produit->save();
